eb75e5c937bbfa25abbb36da86fc4e00e69cdad1
asus <asus@DESKTOP-9UKBS01>
2022-04-04 18:32:04 +0700

    Add project files.

---
+++ AssignmentWeek7/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace AssignmentWeek7
+{
+    partial class FormQuizPanda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMasukkanKalimat = new System.Windows.Forms.Label();
+            this.lblMasukkanHuruf = new System.Windows.Forms.Label();
+            this.txtBxMasukkanKalimat = new System.Windows.Forms.TextBox();
+            this.txtBxMasukkanHuruf = new System.Windows.Forms.TextBox();
+            this.lblMenjadi = new System.Windows.Forms.Label();
+            this.txtBxMenjadi = new System.Windows.Forms.TextBox();
+            this.btnKonversi = new System.Windows.Forms.Button();
+            this.lblHasil = new System.Windows.Forms.Label();
+            this.lblHasilOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblMasukkanKalimat
+            // 
+            this.lblMasukkanKalimat.AutoSize = true;
+            this.lblMasukkanKalimat.Location = new System.Drawing.Point(22, 29);
+            this.lblMasukkanKalimat.Name = "lblMasukkanKalimat";
+            this.lblMasukkanKalimat.Size = new System.Drawing.Size(131, 19);
+            this.lblMasukkanKalimat.TabIndex = 0;
+            this.lblMasukkanKalimat.Text = "Masukkan Kalimat :";
+            // 
+            // lblMasukkanHuruf
+            // 
+            this.lblMasukkanHuruf.AutoSize = true;
+            this.lblMasukkanHuruf.Location = new System.Drawing.Point(22, 91);
+            this.lblMasukkanHuruf.Name = "lblMasukkanHuruf";
+            this.lblMasukkanHuruf.Size = new System.Drawing.Size(118, 19);
+            this.lblMasukkanHuruf.TabIndex = 1;
+            this.lblMasukkanHuruf.Text = "Masukkan Huruf :";
+            // 
+            // txtBxMasukkanKalimat
+            // 
+            this.txtBxMasukkanKalimat.Location = new System.Drawing.Point(155, 29);
+            this.txtBxMasukkanKalimat.Name = "txtBxMasukkanKalimat";
+            this.txtBxMasukkanKalimat.Size = new System.Drawing.Size(345, 26);
+            this.txtBxMasukkanKalimat.TabIndex = 2;
+            // 
+            // txtBxMasukkanHuruf
+            // 
+            this.txtBxMasukkanHuruf.Location = new System.Drawing.Point(155, 91);
+            this.txtBxMasukkanHuruf.Name = "txtBxMasukkanHuruf";
+            this.txtBxMasukkanHuruf.Size = new System.Drawing.Size(100, 26);
+            this.txtBxMasukkanHuruf.TabIndex = 3;
+            // 
+            // lblMenjadi
+            // 
+            this.lblMenjadi.AutoSize = true;
+            this.lblMenjadi.Location = new System.Drawing.Point(332, 91);
+            this.lblMenjadi.Name = "lblMenjadi";
+            this.lblMenjadi.Size = new System.Drawing.Size(62, 19);
+            this.lblMenjadi.TabIndex = 4;
+            this.lblMenjadi.Text = "Menjadi:";
+            // 
+            // txtBxMenjadi
+            // 
+            this.txtBxMenjadi.Location = new System.Drawing.Point(400, 91);
+            this.txtBxMenjadi.Name = "txtBxMenjadi";
+            this.txtBxMenjadi.Size = new System.Drawing.Size(100, 26);
+            this.txtBxMenjadi.TabIndex = 5;
+            // 
+            // btnKonversi
+            // 
+            this.btnKonversi.Location = new System.Drawing.Point(155, 155);
+            this.btnKonversi.Name = "btnKonversi";
+            this.btnKonversi.Size = new System.Drawing.Size(345, 23);
+            this.btnKonversi.TabIndex = 6;
+            this.btnKonversi.Text = "Konversi";
+            this.btnKonversi.UseVisualStyleBackColor = true;
+            this.btnKonversi.Click += new System.EventHandler(this.btnKonversi_Click);
+            // 
+            // lblHasil
+            // 
+            this.lblHasil.AutoSize = true;
+            this.lblHasil.Location = new System.Drawing.Point(22, 220);
+            this.lblHasil.Name = "lblHasil";
+            this.lblHasil.Size = new System.Drawing.Size(50, 19);
+            this.lblHasil.TabIndex = 7;
+            this.lblHasil.Text = "Hasil : ";
+            // 
+            // lblHasilOutput
+            // 
+            this.lblHasilOutput.AutoSize = true;
+            this.lblHasilOutput.Location = new System.Drawing.Point(151, 220);
+            this.lblHasilOutput.Name = "lblHasilOutput";
+            this.lblHasilOutput.Size = new System.Drawing.Size(29, 19);
+            this.lblHasilOutput.TabIndex = 8;
+            this.lblHasilOutput.Text = ".....";
+            this.lblHasilOutput.Click += new System.EventHandler(this.lblHasilOutput_Click);
+            // 
+            // FormQuizPanda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 280);
+            this.Controls.Add(this.lblHasilOutput);
+            this.Controls.Add(this.lblHasil);
+            this.Controls.Add(this.btnKonversi);
+            this.Controls.Add(this.txtBxMenjadi);
+            this.Controls.Add(this.lblMenjadi);
+            this.Controls.Add(this.txtBxMasukkanHuruf);
+            this.Controls.Add(this.txtBxMasukkanKalimat);
+            this.Controls.Add(this.lblMasukkanHuruf);
+            this.Controls.Add(this.lblMasukkanKalimat);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormQuizPanda";
+            this.Text = "Quiz Panda";
+            this.Load += new System.EventHandler(this.FormQuizPanda_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMasukkanKalimat;
+        private System.Windows.Forms.Label lblMasukkanHuruf;
+        private System.Windows.Forms.TextBox txtBxMasukkanKalimat;
+        private System.Windows.Forms.TextBox txtBxMasukkanHuruf;
+        private System.Windows.Forms.Label lblMenjadi;
+        private System.Windows.Forms.TextBox txtBxMenjadi;
+        private System.Windows.Forms.Button btnKonversi;
+        private System.Windows.Forms.Label lblHasil;
+        private System.Windows.Forms.Label lblHasilOutput;
+    }
+}
+
---
+++ AssignmentWeek7/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AssignmentWeek7
+{
+    public partial class FormQuizPanda : Form
+    {
+        public string[] ArrayHuruf = new string[30];
+
+
+        public FormQuizPanda()
+        {
+            InitializeComponent();
+        }
+
+        private void lblHasilOutput_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnKonversi_Click(object sender, EventArgs e)
+        {
+            lblHasilOutput.Text = ConvertToUpperCase(txtBxMasukkanKalimat.Text);
+
+            //i'm giving up i'm so sorry, this is actually the value of the alphabet, but still can't 
+            if (txtBxMenjadi.Text == "A")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[0];
+            }
+            else if (txtBxMenjadi.Text == "B")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[1];
+            }
+            else if (txtBxMenjadi.Text == "C")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[2];
+            }
+            else if (txtBxMenjadi.Text == "D")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[3];
+            }
+            else if (txtBxMenjadi.Text == "E")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[4];
+            }
+            else if (txtBxMenjadi.Text == "F")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[5];
+            }
+            else if (txtBxMenjadi.Text == "G")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[6];
+            }
+            else if (txtBxMenjadi.Text == "H")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[7];
+            }
+            else if (txtBxMenjadi.Text == "I")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[8];
+            }
+            else if (txtBxMenjadi.Text == "J")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[9];
+            }
+            else if (txtBxMenjadi.Text == "K")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[10];
+            }
+            else if (txtBxMenjadi.Text == "L")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[11];
+            }
+            else if (txtBxMenjadi.Text == "M")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[12];
+            }
+            else if (txtBxMenjadi.Text == "N")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[13];
+            }
+            else if (txtBxMenjadi.Text == "O")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[14];
+            }
+            else if (txtBxMenjadi.Text == "P")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[15];
+            }
+            else if (txtBxMenjadi.Text == "Q")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[16];
+            }
+            else if (txtBxMenjadi.Text == "R")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[17];
+            }
+            else if (txtBxMenjadi.Text == "S")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[18];
+            }
+            else if (txtBxMenjadi.Text == "T")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[19];
+            }
+            else if (txtBxMenjadi.Text == "U")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[20];
+            }
+            else if (txtBxMenjadi.Text == "V")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[21];
+            }
+            else if (txtBxMenjadi.Text == "W")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[22];
+            }
+            else if (txtBxMenjadi.Text == "X")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[23];
+            }
+            else if (txtBxMenjadi.Text == "Y")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[24];
+            }
+            else if (txtBxMenjadi.Text == "Z")
+            {
+                txtBxMasukkanHuruf.Text = ArrayHuruf[25];
+            }
+
+        }
+
+        public static string ConvertToUpperCase(string input)
+        {
+            //for converting the word to be a capital latter 
+            string output = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] >= 'a' && input[i] <= 'z')
+                {
+                    output += (char)(input[i] - 'a' + 'A');
+                }
+                else
+                {
+                    output += input[i];
+                }
+            }
+            return output;
+        }
+
+        private void FormQuizPanda_Load(object sender, EventArgs e)
+        {
+            //i'm giving up i'm so sorry, this is actually the value of the alphabet, but still can't 
+            ArrayHuruf[0] = "A";
+            ArrayHuruf[1] = "B";
+            ArrayHuruf[2] = "C";
+            ArrayHuruf[3] = "D";
+            ArrayHuruf[4] = "E";
+            ArrayHuruf[5] = "F";
+            ArrayHuruf[6] = "G";
+            ArrayHuruf[7] = "H";
+            ArrayHuruf[8] = "I";
+            ArrayHuruf[9] = "J";
+            ArrayHuruf[10] = "K";
+            ArrayHuruf[11] = "L";
+            ArrayHuruf[12] = "M";
+            ArrayHuruf[13] = "N";
+            ArrayHuruf[14] = "O";
+            ArrayHuruf[15] = "P";
+            ArrayHuruf[16] = "Q";
+            ArrayHuruf[17] = "R";
+            ArrayHuruf[18] = "S";
+            ArrayHuruf[19] = "T";
+            ArrayHuruf[20] = "U";
+            ArrayHuruf[21] = "V";
+            ArrayHuruf[22] = "W";
+            ArrayHuruf[23] = "X";
+            ArrayHuruf[24] = "Y";
+            ArrayHuruf[25] = "Z";
+
+
+        }
+    }
+}
